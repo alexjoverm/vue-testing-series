06946c5dd953b5012cfd20cd7143163f9a412e94
Alex J <alexjovermorales@gmail.com>
2018-08-27 19:43:06 +0200

    Remove nextTick references

--- test/Form.test.js
+++ test/Form.test.js
@@ -1,82 +1,73 @@
-
-
 // jest.mock('axios', () => ({
 //   get: jest.fn(() => Promise.resolve({ data: 3 }))
 // }))
 
-import { shallowMount } from '@vue/test-utils'
-import Form from '../src/components/Form'
-import axios from 'axios'
+import { shallowMount } from "@vue/test-utils";
+import Form from "../src/components/Form";
+import axios from "axios";
 
-describe('Form.test.js', () => {
-  let cmp
+describe("Form.test.js", () => {
+  let cmp;
 
   beforeEach(() => {
-    cmp = shallowMount(Form)
-    jest.resetModules()
-    jest.clearAllMocks()
-  })
+    cmp = shallowMount(Form);
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
 
-  it('Calls axios.get', async () => {
-    const result = await cmp.vm.onSubmit('an')
+  it("Calls axios.get", async () => {
+    const result = await cmp.vm.onSubmit("an");
 
-    expect(result).toEqual({ data: [3] })
-    expect(cmp.vm.results).toEqual([3])
-    expect(axios.get).toBeCalledWith('https://jsonplaceholder.typicode.com/posts?q=an')
-  })
+    expect(result).toEqual({ data: [3] });
+    expect(cmp.vm.results).toEqual([3]);
+    expect(axios.get).toBeCalledWith(
+      "https://jsonplaceholder.typicode.com/posts?q=an"
+    );
+  });
 
-  describe('Properties', () => {
-    it('returns the string in normal order if reversed property is not true', () => {
-      cmp.setData({ inputValue: 'Yoo' })
+  describe("Properties", () => {
+    it("returns the string in normal order if reversed property is not true", () => {
+      cmp.setData({ inputValue: "Yoo" });
 
-      expect(cmp.vm.reversedInput).toBe('Yoo')
-      expect(axios.get).not.toBeCalled()
-    })
+      expect(cmp.vm.reversedInput).toBe("Yoo");
+      expect(axios.get).not.toBeCalled();
+    });
 
-    it('returns the reversed string if reversed property is true', () => {
-      cmp.setData({ inputValue: 'Yoo' })
-      cmp.setProps({ reversed: true })
+    it("returns the reversed string if reversed property is true", () => {
+      cmp.setData({ inputValue: "Yoo" });
+      cmp.setProps({ reversed: true });
 
-      expect(cmp.vm.reversedInput).toBe('ooY')
-    })
-  })
+      expect(cmp.vm.reversedInput).toBe("ooY");
+    });
+  });
 
-  describe('Watchers - inputValue', () => {
-    let spy
+  describe("Watchers - inputValue", () => {
+    let spy;
 
     beforeAll(() => {
-      spy = jest.spyOn(console, 'log')
-    })
+      spy = jest.spyOn(console, "log");
+    });
 
     afterEach(() => {
-      spy.mockClear()
-    })
+      spy.mockClear();
+    });
 
-    it('is not called if value is empty (trimmed)', next => {
-      cmp.setData({ inputValue: '   ' })
-      cmp.vm.$nextTick(() => {
-        expect(spy).not.toBeCalled()
-        next()
-      })
-    })
+    it("is not called if value is empty (trimmed)", () => {
+      cmp.setData({ inputValue: "   " });
+      expect(spy).not.toBeCalled();
+    });
 
-    it('is not called if values are the same', next => {
+    it("is not called if values are the same", () => {
       cmp = shallowMount(Form, {
-        data: () => ({ inputValue: 'foo' })
-      })
-      cmp.setData({ inputValue: 'foo' })
-      cmp.vm.$nextTick(() => {
-        expect(spy).not.toBeCalled()
-        next()
-      })
-    })
+        data: () => ({ inputValue: "foo" })
+      });
+      cmp.setData({ inputValue: "foo" });
+      expect(spy).not.toBeCalled();
+    });
 
-    it('is called with the new value in other cases', next => {
-      cmp.setData({ inputValue: 'foo' })
-      cmp.vm.$nextTick(() => {
-        expect(spy).toBeCalled()
-        next()
-      })
-    })
-  })
-})
+    it("is called with the new value in other cases", () => {
+      cmp.setData({ inputValue: "foo" });
+      expect(spy).toBeCalled();
+    });
+  });
+});
